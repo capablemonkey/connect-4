afdb8da76c79d665b33a00dbc64a72df642fe1a7
Gordon Zheng <gordon@greenhouse.io>
2016-12-08 15:42:25 -0500

    Implement board generator

--- src/connect_4/core.clj
+++ src/connect_4/core.clj
@@ -1,7 +1,23 @@
 (ns connect-4.core
   (:gen-class))
 
+(defn new-board [rows columns]
+  "Returns a vector containing *rows* number of vectors, each of length *columns* with cells initialized to nil."
+  (map
+    (fn [row] (vec (repeat columns nil)))
+    (vec (repeat rows nil))))
+
+(defn drop-disc [board column color]
+  "Returns a new board with a disc of the given color added to the top of the given column")
+
+(defn won? [board]
+  "Returns true if the board is in a win state")
+
+(defn tie? [board]
+  "Returns true if there are no more moves left in the board")
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (new-board 6 7)))
--- test/connect_4/core_test.clj
+++ test/connect_4/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [connect-4.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest board-test
+  (testing "new-board returns a valid board"
+    (is
+      (=
+        6
+        (count (new-board 6 7)))
+      "board contains the correct number of rows")
+    (is
+      (=
+        7
+        (count (first (new-board 6 7))))
+      "board contains the correct number of columns")))
